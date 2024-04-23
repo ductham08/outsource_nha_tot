20bd7a8cdc1b6b4f7d3ff6b1a0346c49cedd414a
ductham08 <ductham784@gmail.com>
2024-04-24 00:34:27 +0700

    update src phong tro

--- publics/js/sendEmail.js
+++ publics/js/sendEmail.js
@@ -2,18 +2,46 @@ emailjs.init("zoonWG4Iy1d8NHZyc");
 
 $('#btnSubmit').click(function(event) {
 
+    event.preventDefault()  ;
+
+    var userName = $("#user-name").val();
+    var phoneNumber = $("#phone-number").val();
+    var address = $("#address").val();
+    var appointmentDate = $("#input-buy-home").val();
+    var locationValue = $("#location").html();
+
+    const message = `
+        <!DOCTYPE html>
+        <html lang="en">
+        <head>
+            <meta charset="UTF-8">
+            <style>
+                p{
+                    margin-bottom: 0;
+                }
+            </style>
+        </head>
+            <body>
+                <p><b>Địa chỉ phòng:</b> ${locationValue}</p>
+                <p><b>Họ và tên:</b> ${userName}</p>
+                <p><b>Ngày hẹn:</b> ${appointmentDate}</p>
+                <p><b>Số điện thoại:</b> ${phoneNumber}</p>
+                <p><b>Địa chỉ tìm phòng:</b> ${address}</p>
+            </body>
+        </html>
+    `
+
     var templateParams = {
-        message: "check account",
-        to_email: 'thamndph15221@fpt.edu.vn'
+        message: message,
     };
 
-
-    emailjs.send('service_o3xkrqq', 'template_f2mji4p', templateParams )
-    .then(function(response) {
-        console.log('Email sent successfully', response);
-        $('#response').text('Email sent successfully');
-    }, function(error) {
-        console.error('Error sending email', error);
-        $('#response').text('Error sending email');
-    });
+    emailjs.send('service_l5dq556', 'template_f2mji4p', templateParams )
+        .then(
+            function(response) {
+                toastr.success('Gửi thông tin thành công!');
+            }, 
+            function(error) {
+                return toastr.error("Gửi thông tin không thành công!");
+            }
+        );
 });
